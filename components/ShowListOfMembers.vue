70532f88913b37a484b196bad6332e17c54228d7
Samuël Lefebvre <SamLefebvre@users.noreply.github.com>
2022-09-11 17:05:34 -0400

    Add files via upload

---
+++ components/ShowListOfMembers.vue
@@ -0,0 +1,91 @@
+<template>
+
+<div>
+    <h2 v-if="lang ==='en'">{{ executiveLang.en }}</h2><h2 v-else>{{ executiveLang.fr }}</h2>
+    <main class="container">
+        <div class="member" v-for="member in adminMembers">
+            <img v-bind:src="$withBase('/members/' + member.pictureName)" class="members">
+            <p class="clsMember" v-if="lang ==='en'">• {{ member.descriptionEn }} •</p><p class="clsMember" v-else>• {{ member.descriptionFr }} •</p>
+            <p class="clsMember">{{ member.name }}</p>
+        </div>
+    </main>
+    <h2 v-if="lang ==='en'">{{ teamLeaderLang.en }}</h2><h2 v-else>{{ teamLeaderLang.fr }}</h2>
+    <main class="container">
+        <div class="member" v-for="member in teamLeader">
+            <img v-bind:src="$withBase('/members/' + member.pictureName)" class="members">
+            <p class="clsMember" v-if="lang ==='en'">• {{ member.descriptionEn }} •</p><p class="clsMember" v-else>• {{ member.descriptionFr }} •</p>
+            <p class="clsMember">{{ member.name }}</p>
+        </div>
+    </main>
+</div>
+
+</template>
+
+<script>
+    export default {
+        props: ['lang'],
+        data() {
+            return {
+                executiveLang: {
+                    fr: "Exécutif",
+                    en: "Executive",
+                },
+                teamLeaderLang: {
+                    fr: "Directeurs",
+                    en: "Directors",
+                },
+                msg: "Liste des membres",
+                adminMembers: [
+                    { name: 'Alexandre-Xavier Panu-bule', descriptionFr:'Capitaine', descriptionEn:'Captain', pictureName: 'alexandre-xavier_panu-bule.jpg' },
+                    { name: 'Mubashar Hussain ', descriptionFr:'Co-capitaine et Directeur Électronique', descriptionEn:'Co-Capitain and Electronic Director', pictureName: 'mubashar_hussain.png' },
+                    { name: 'Jacques Veneziano ', descriptionFr:'Trésorier et chef d’équipe pour MICSA', descriptionEn:'Treasurer and MICSA Team Lead', pictureName: 'jacques_veneziano.png' }
+                ],
+                teamLeader: [
+                    { name: 'Interim', descriptionFr: 'Directeur Logiciel', descriptionEn: 'Software Director', pictureName: 'missing.png' },
+                    { name: 'Interim', descriptionFr: 'Directeur Multimédia', descriptionEn: 'Multimedia Director', pictureName: 'missing.png' },
+                    { name: 'Jean-Rémi Martin', descriptionFr: 'Directeur Mécanique', descriptionEn: 'Mechanical Director', pictureName: 'jean-remi_martin.png' },
+                    { name: 'Mubashar Hussain', descriptionFr: 'Membre et co-directeur mécanique', descriptionEn: 'Co-Mechanical Director', pictureName: 'mubashar_hussain.png' }
+                ]
+            }
+        }
+    };
+</script>
+
+
+
+<style scoped>
+div.member {
+    font-size: 20px;
+    text-align: center;
+    float: left;
+    padding-left: 10px;
+    padding-right: 10px;
+}
+
+p.clsMember {
+    line-height: 5px;
+}
+
+img.members {
+
+    width: 215px;
+    height: 215px;
+}
+.container {
+    display:flex;
+    flex-wrap: wrap;
+}
+.container>* {
+        flex: 1 1 250px;
+    }
+
+main{
+    overflow: hidden;
+}
+
+main div:nth-child(4){
+    clear: both;
+}
+
+
+</style>
---
+++ components/ShowListOfPartners.vue
@@ -0,0 +1,45 @@
+<template>
+    <div class="partners">
+        <main>
+            <div v-for="partner in partners">
+                <a target="_blank" :href="partner.url"><img v-bind:src="'/partenaires/' + partner.pictureName" :title="partner.name" :alt="partner.name" :longdesc="partner.description"></a>
+            </div>
+        </main>
+    </div>
+
+</template>
+
+<script>
+    export default {
+        data() {
+            return {
+                partners: [
+                    { name: 'ÉTS', description: 'École de Technologie Supérieure', pictureName: 'ets.png', url:'https://www.etsmtl.ca/' },
+                    { name: 'OpenBCI', description: 'OpenBCI - Open Source Biosensing Tools (EEG, EMG, EKG, and more)', pictureName: 'openbci.png', url:'http://openbci.com/' },
+                    { name: 'NeuroTechMTL', description: 'NeuroTechX', pictureName: 'neurotechmtl.png', url:'http://neurotechx.com' },
+                    { name: 'Altium', description: 'PCB Design Software | Innovation For PCB Design', pictureName: 'altium.png', url: 'https://www.altium.com/' },
+                    { name: 'Cedille', description: 'Club Cedille', pictureName: 'cedille.png', url:'http://cedille.etsmtl.ca/' },
+                    { name: 'CRITIAS', description: 'NSERC-EERS Industrial Research Chair in In-Ear Technologies', pictureName: 'critias.png', url:'http://critias.etsmtl.ca/' },
+                    { name: 'SolidWorks', description: 'Dassault Systèmes SolidWorks Corp', pictureName: 'solidworks.svg', url:'https://www.solidworks.com/' }
+                ]
+            }
+        }
+    };
+</script>
+
+<style scoped>
+    main{
+        column-count: 3;
+        column-gap: 4.5rem;
+        padding: 0.5rem;
+    }
+    img{
+        max-width:128px;
+        max-height:128px;
+        width:auto;
+        height:auto;
+    }
+    div.partners{
+        text-align: center;
+    }
+</style>
